8a8177df9016a0f46607609aca28ce116e8181b4
Yanis Falaki <yanisfalaki@outlook.com>
2022-09-25 17:36:33 -0400

    Added tetraminos and gameHelper
    Tetraminos are the blocks in tetris. the gameHelper script is used (so far) to generate a 2d array for the stage

--- src/components/Stage.jsx
+++ src/components/Stage.jsx
@@ -4,7 +4,9 @@ import { Cell } from "./Cell";
 export const Stage = ({ stage }) => {
   return (
     <div>
-      <Cell />
+      {
+        stage.map((row) => row.map((cell, x) => <Cell key={x} type={cell[0]} />))
+      }
     </div>
   );
 };
